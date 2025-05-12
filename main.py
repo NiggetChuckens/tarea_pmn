ab5bd3fedad79b27f1b08199e98b2260c9176b9f
Rodrigo <83593684+NiggetChuckens@users.noreply.github.com>
2025-05-11 22:03:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -98,4 +98,4 @@ def get_user_history(user_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
